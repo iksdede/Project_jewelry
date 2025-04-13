cc0b8b2ce10e202c18bb2138c6c0a7668d99b0e8
iksdede <maxxxandcomp@gmail.com>
2025-04-14 01:10:43 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
         modal.style.display = 'none';
       }
     });
-  });
+  
 
 function scrollToTop() {
     window.scrollTo({
@@ -112,3 +112,20 @@ function scrollToTop() {
       }
     });
   });
+
+  const preloader = document.querySelector('.preloader');
+  const content = document.querySelector('.content');
+  if (preloader && content) {
+      setTimeout(() => {
+          // Скрываем прелоадер
+          preloader.style.opacity = '0';
+          preloader.style.visibility = 'hidden';
+
+          // Показываем контент
+          content.style.display = 'block';
+
+          // Удаляем элемент из DOM
+          preloader.remove();
+      }, 3000); // Задержка 3 секунды
+  }
+});
